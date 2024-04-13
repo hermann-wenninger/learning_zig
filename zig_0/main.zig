184bcd7ac6e0e264e03f81d706f971a071b35063
hermann wenninger <info@wennhmann.de>
2024-04-13 17:56:30 +0200

    cap_0

---
+++ zig_0/main.zig
@@ -0,0 +1,13 @@
+const std = @import("std");
+
+// "info" now refers to the "std.log.info" function.
+const info = std.log.info;
+
+// Usual hello world.
+// syntax: [pub] fn <function-name>(<arguments>) <return-type> { <body> }
+pub fn main() void {
+    // Contrary to C functions, Zig functions have a fixed number of arguments.
+    // In C: "printf" takes any number of arguments.
+    // In Zig: std.log.info takes a format and a list of elements to print.
+    info("hello world", .{});  // .{} is an empty anonymous tuple.
+}
